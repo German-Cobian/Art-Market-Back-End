67525b712521cfe69f7291f00b0bd31d5cb738df
German Renato Cobian <germancobian@hotmail.com>
2023-01-25 14:19:13 -0800

    Create devise User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :v1, defaults: { format: 'json' } do
     resources :creations, only: [:index, :show, :create, :destroy]
     resources :purchases, only: [:index, :show, :create, :destroy]
