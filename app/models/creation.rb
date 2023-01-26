8a3a279a2eaa0e63c6bd3dbd2a4f9ab86eca655f
German Renato Cobian <germancobian@hotmail.com>
2023-01-26 14:26:50 -0800

    Add image_url and associate it to the Creation model

--- app/models/creation.rb
+++ app/models/creation.rb
@@ -1,5 +1,6 @@
 class Creation < ApplicationRecord
   has_many :purchases, dependent: :destroy
+  has_one_attached :creation_pic
 
   validates :artist, presence: true
   validates :title, presence: true
@@ -10,4 +11,8 @@ class Creation < ApplicationRecord
   validates :art_category, presence: true
   validates :country_of_origin, presence: true
   validates :price, presence: true
+
+  def image_url
+    Rails.application.routes.url_helpers.url_for(creation_pic) if creation_pic.attached?
+  end
 end
